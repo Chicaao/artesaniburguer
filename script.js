fbc62bb3a64cd2f9fe5b0c56fe46cd67a483dac7
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-06-09 12:50:06 -0300

    Correção de erros

--- script.js
+++ script.js
@@ -6,16 +6,23 @@
 
   if (menuToggle && mainNav) {
     menuToggle.addEventListener('click', function() {
-        mainNav.classList.toggle('is-open')
-
+        mainNav.classList.toggle('is-open');
         const isExpanded = mainNav.classList.contains('is-open');
         menuToggle.setAttribute('aria-expanded', isExpanded);
 
         const icon = menuToggle.querySelector('i');
+
+        // --- INÍCIO DA DEPURAÇÃO ---
+            console.log('Elemento do ícone encontrado:', icon);
+            console.log('O menu está aberto?', isExpanded);
+        // --- FIM DA DEPURAÇÃO ---
+
         if (isExpanded) {
+            console.log('Tentando trocar para o ícone X (fa-times)');
             icon.classList.remove('fa-bars');
             icon.classList.add('fa-times')
         } else {
+            console.log('Tentando trocar para o ícone de barras (fa-bars)');
             icon.classList.remove('fa-times');
             icon.classList.add('fa-bars');
         }
