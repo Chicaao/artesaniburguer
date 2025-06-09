e5c3426211951dda7a640c58478ff2dd92e32870
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-06-09 12:11:52 -0300

    Menu hamburguer e responsividade mobile

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
  document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+  // --- Script para o Menu Hambúrguer ---
+  const menuToggle = document.querySelector('.menu-toggle')
+  const mainNav = document.querySelector('.main-nav')
+
+  if (menuToggle && mainNav) {
+    menuToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('is-open')
+
+        const isExpanded = mainNav.classList.contains('is-open');
+        menuToggle.setAttribute('aria-expanded', isExpanded);
+
+        const icon = menuToggle.querySelector('i');
+        if (isExpanded) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times')
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    })
+  }
